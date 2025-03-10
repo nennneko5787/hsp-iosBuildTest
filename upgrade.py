b982c0e49257e1b853375e53029058f84491e0e8
Nennneko5787 <nennneko5787@gmail.com>
2025-03-10 18:37:41 +0900

    a

--- upgrade.py
+++ upgrade.py
@@ -4,20 +4,45 @@ from pbxproj import XcodeProject
 from pbxproj.pbxsections.PBXFileReference import PBXFileReference
 
 project = XcodeProject.load("hspproj.xcodeproj/project.pbxproj")
-
-folder = "iHSP30"
 bs = "\\"
 
+folder = "iHSP30"
 for root, _, files in os.walk(folder):
     for file in files:
         rel_path = os.path.relpath(os.path.join(root, file), folder)
         for file in project.get_files_by_name(file):
             file: PBXFileReference = file
             project.remove_file_by_id(file.get_id())
-        for file in project.get_files_by_path(f"iHSP30/{rel_path.replace(bs, '/')}"):
+        for file in project.get_files_by_path(f"{folder}/{rel_path.replace(bs, '/')}"):
             file: PBXFileReference = file
             project.remove_file_by_id(file.get_id())
-        project.add_file(f"iHSP30/{rel_path.replace(bs, '/')}")
+        project.add_file(f"{folder}/{rel_path.replace(bs, '/')}")
+        print(rel_path)
+
+folder = "gameplay"
+for root, _, files in os.walk(folder):
+    for file in files:
+        rel_path = os.path.relpath(os.path.join(root, file), folder)
+        for file in project.get_files_by_name(file):
+            file: PBXFileReference = file
+            project.remove_file_by_id(file.get_id())
+        for file in project.get_files_by_path(f"{folder}/{rel_path.replace(bs, '/')}"):
+            file: PBXFileReference = file
+            project.remove_file_by_id(file.get_id())
+        project.add_file(f"{folder}/{rel_path.replace(bs, '/')}")
+        print(rel_path)
+
+folder = "external-deps"
+for root, _, files in os.walk(folder):
+    for file in files:
+        rel_path = os.path.relpath(os.path.join(root, file), folder)
+        for file in project.get_files_by_name(file):
+            file: PBXFileReference = file
+            project.remove_file_by_id(file.get_id())
+        for file in project.get_files_by_path(f"{folder}/{rel_path.replace(bs, '/')}"):
+            file: PBXFileReference = file
+            project.remove_file_by_id(file.get_id())
+        project.add_file(f"{folder}/{rel_path.replace(bs, '/')}")
         print(rel_path)
 
 
