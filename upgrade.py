05fce8b6a0a6beae968ac94ff2acd35461d5cfaa
Nennneko5787 <nennneko5787@gmail.com>
2025-03-10 19:18:02 +0900

    NG

--- upgrade.py
+++ upgrade.py
@@ -34,5 +34,19 @@ for root, _, files in os.walk(folder):
         project.add_file(f"{folder}/{rel_path.replace(bs, '/')}")
         print(rel_path)
 
+folder = "external-deps"
+for root, _, files in os.walk(folder):
+    for _file in files:
+        if _file.endswith(".dox"):
+            continue
+        rel_path = os.path.relpath(os.path.join(root, _file), folder)
+        for file in project.get_files_by_name(_file):
+            file: PBXFileReference = file
+            project.remove_file_by_id(file.get_id())
+        for file in project.get_files_by_path(f"{folder}/{rel_path.replace(bs, '/')}"):
+            file: PBXFileReference = file
+            project.remove_file_by_id(file.get_id())
+        project.add_file(f"{folder}/{rel_path.replace(bs, '/')}")
+        print(rel_path)
 
 project.save()
