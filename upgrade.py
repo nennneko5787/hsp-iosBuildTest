3f888120a8d03340a7dc31253458153a362416da
Nennneko5787 <nennneko5787@gmail.com>
2025-03-10 18:21:38 +0900

    a

--- upgrade.py
+++ upgrade.py
@@ -1,13 +1,24 @@
+import os
+
 from pbxproj import XcodeProject
 from pbxproj.pbxsections.PBXFileReference import PBXFileReference
 
 project = XcodeProject.load("hspproj.xcodeproj/project.pbxproj")
 
-project.add_file("iHSP30/obaq/physics/vessel.h")
-project.remove_file_by_id("1E4AFEAD25E00F4700213810")
-print(project.get_files_by_name("vessel.h"))
-for file in project.get_files_by_name("vessel.h"):
-    file: PBXFileReference = file
-    print(file.get_id())
+folder = "iHSP30"
+bs = "\\"
+
+for root, _, files in os.walk(folder):
+    for file in files:
+        rel_path = os.path.relpath(os.path.join(root, file), folder)
+        for file in project.get_files_by_name(file):
+            file: PBXFileReference = file
+            project.remove_file_by_id(file.get_id())
+        for file in project.get_files_by_path(f"iHSP30/{rel_path.replace(bs, '/')}"):
+            file: PBXFileReference = file
+            project.remove_file_by_id(file.get_id())
+        project.add_file(f"iHSP30/{rel_path.replace(bs, '/')}")
+        print(rel_path)
+
 
 project.save()
